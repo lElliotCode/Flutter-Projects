6f43261160138b225809237ca7267b36ffe98108
Elliot <svazquez@airata.net>
2026-06-17 16:42:48 -0300

    search delegate, suggestions

--- cinemapedia/lib/infrastructure/models/moviedb/movie_moviedb.dart
+++ cinemapedia/lib/infrastructure/models/moviedb/movie_moviedb.dart
@@ -27,30 +27,32 @@ class MovieFromMovieDB {
   final String overview;
   final double popularity;
   final String posterPath;
-  final DateTime releaseDate;
+  final DateTime? releaseDate;
   final bool softcore;
   final bool video;
   final double voteAverage;
   final int voteCount;
 
-
-  factory MovieFromMovieDB.fromJson(Map<String, dynamic> json) => MovieFromMovieDB(
-    adult: json["adult"] ?? false,
-    backdropPath: json["backdrop_path"] ?? '',
-    genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-    id: json["id"],
-    title: json["title"],
-    originalLanguage: json["original_language"],
-    originalTitle: json["original_title"],
-    overview: json["overview"] ?? '',
-    popularity: json["popularity"]?.toDouble(),
-    posterPath: json["poster_path"] ?? '',
-    releaseDate: DateTime.parse(json["release_date"]),
-    softcore: json["softcore"],
-    video: json["video"],
-    voteAverage: json["vote_average"]?.toDouble(),
-    voteCount: json["vote_count"],
-  );
+  factory MovieFromMovieDB.fromJson(Map<String, dynamic> json) =>
+      MovieFromMovieDB(
+        adult: json["adult"] ?? false,
+        backdropPath: json["backdrop_path"] ?? '',
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        title: json["title"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"] ?? '',
+        popularity: json["popularity"]?.toDouble(),
+        posterPath: json["poster_path"] ?? '',
+        releaseDate: json["release_date"] != null
+            ? DateTime.parse(json["release_date"])
+            : null,
+        softcore: json["softcore"],
+        video: json["video"],
+        voteAverage: json["vote_average"]?.toDouble(),
+        voteCount: json["vote_count"],
+      );
 
   Map<String, dynamic> toJson() => {
     "adult": adult,
@@ -63,8 +65,9 @@ class MovieFromMovieDB {
     "overview": overview,
     "popularity": popularity,
     "poster_path": posterPath,
-    "release_date":
-        "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+    "release_date": (releaseDate != null)
+        ? "${releaseDate!.year.toString().padLeft(4, '0')}-${releaseDate!.month.toString().padLeft(2, '0')}-${releaseDate!.day.toString().padLeft(2, '0')}"
+        : null,
     "softcore": softcore,
     "video": video,
     "vote_average": voteAverage,
--- cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -2,7 +2,13 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
   @override
   String get searchFieldLabel => 'Buscar película';
 
@@ -34,6 +40,88 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('Build Suggestions');
+    return FutureBuilder(
+      future: searchMovies(query),
+      initialData: const [],
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final Movie movie = movies[index];
+            return _MovieItem(movie: movie, onMovieSelected: close);
+          },
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  final Function onMovieSelected;
+
+  const _MovieItem({required this.movie, required this.onMovieSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 20),
+
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style: textStyles.titleMedium),
+                  (movie.overview.length > 200)
+                      ? Text('${movie.overview.substring(0, 200)}...')
+                      : Text(movie.overview),
+
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_outlined,
+                        color: Colors.yellow.shade800,
+                      ),
+                      const SizedBox(width: 3),
+                      Text(
+                        '${(movie.voteAverage * 10).truncateToDouble() / 10}',
+                        style: textStyles.bodyMedium?.copyWith(
+                          color: Colors.yellow.shade900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
