9529b6f8cf8c9542fd2eb39f8aaf7f2e3ae08366
Elliot <svazquez@airata.net>
2026-07-14 12:46:15 -0300

    teslo app login

---
+++ teslo-app/lib/features/auth/infra/infrastructure.dart
@@ -0,0 +1,4 @@
+export 'datasources/auth_datasource_impl.dart';
+export 'mappers/mapper.dart';
+export 'repositories/auth_repository_impl.dart';
+export 'errors/auth_errors.dart';
---
+++ teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infra/repositories/auth_repository_impl.dart';
+
+final authProvider =
+    StateNotifierProvider.autoDispose<AuthNotifier, AuthState>((ref) {
+  final authRepository = AuthRepositoryImpl();
+
+  return AuthNotifier(authRepository: authRepository);
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+
+  AuthNotifier({required this.authRepository}) : super(AuthState());
+
+  void loginUser(String email, String password) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } catch (e) {
+      logout( ' Invalid user credentials');
+    }
+    // final user = await authRepository.login(email, password);
+    // state = state.copyWith(user: user, authStatus: AuthStatus.authenticated);
+  }
+
+  void registerStatus(String email, String password) async {}
+
+  void checkAuthStatus() async {}
+
+  void _setLoggedUser(User user) {
+    // TODO: save token on device
+    state = state.copyWith(
+        user: user, errorMessage: '', authStatus: AuthStatus.authenticated);
+  }
+
+  Future<void> logout(String? errorMessage) async {
+    // TODO: clear token
+    state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage,
+    );
+  }
+}
+
+enum AuthStatus { checking, authenticated, notAuthenticated }
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String? errorMessage;
+
+  AuthState(
+      {this.authStatus = AuthStatus.checking,
+      this.user,
+      this.errorMessage = ''});
+
+  AuthState copyWith(
+          {AuthStatus? authStatus, User? user, String? errorMessage}) =>
+      AuthState(
+        authStatus: authStatus ?? this.authStatus,
+        user: user ?? this.user,
+        errorMessage: errorMessage,
+      );
+}
